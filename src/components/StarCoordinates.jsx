db2c03dd6d57c8140b031d999659b443dae201a7
Miguel Ángel <ma.monreal@yahoo.es>
2024-02-15 01:26:21 +0100

    viewBox movement

--- src/components/StarCoordinates.jsx
+++ src/components/StarCoordinates.jsx
@@ -3,6 +3,8 @@ import Axis from "./Axis.jsx";
 import Circle from "./Circle.jsx";
 
 import { buildCartesianVector } from "../utils/vector.js";
+import { useRef } from "react";
+import useDrag from "../hooks/useDrag.js";
 
 const StarCoordinates = ({ height, width }) => {
     let centerX = width / 2;
@@ -28,41 +30,51 @@ const StarCoordinates = ({ height, width }) => {
         })
     }
 
-    return (
-        <svg
-            height={height}
-            width={width}
-            viewBox={`${minX} ${minY} ${width} ${height}`}
-        >
-            <Circle
-                cx={0}
-                cy={0}
-                radius="150"
-                stroke="grey"
-                fill="none"
-            />
-            {vectors && vectors.map(vector =>
-                <Axis
-                    key={vector.id}
-                    vector={{ vector }}
-                />
-            )}
+    const svgRef = useRef();
+    useDrag(svgRef, (event) => {
+        setMinX(minX => minX - event.dx);
+        setMinY(minY => minY - event.dy);
+    });
 
-            <g>
-                {data && data.map(value => {
-                    return (
-                        <Circle
-                            key={value.id}
-                            cx={value.x}
-                            cy={value.y}
-                            radius={5}
-                            stroke="salmon"
-                            fill="lightsalmon"
-                        />
-                    );
-                })}
-            </g>
-        </svg>
+
+    return (
+        <>
+            <svg
+                ref={svgRef}
+                height={height}
+                width={width}
+                viewBox={`${minX} ${minY} ${width} ${height}`}
+            >
+                <Circle
+                    cx={0}
+                    cy={0}
+                    radius="150"
+                    stroke="grey"
+                    fill="none"
+                />
+                {vectors && vectors.map(vector =>
+                    <Axis
+                        key={vector.id}
+                        vector={{ vector }}
+                    />
+                )}
+
+                <g>
+                    {data && data.map(value => {
+                        return (
+                            <Circle
+                                key={value.id}
+                                cx={value.x}
+                                cy={value.y}
+                                radius={5}
+                                stroke="salmon"
+                                fill="lightsalmon"
+                            />
+                        );
+                    })}
+                </g>
+            </svg>
+        </>
     );
 };
 
