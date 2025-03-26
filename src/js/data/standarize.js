e35b40fbd710cd14ecd0d2cfea7c751b3cbf984a
Miguel Ángel Monreal Velasco <developer@mkmonreal.com>
2025-03-26 14:33:28 +0100

    WIP - use math js for pca calculation

--- src/js/data/standarize.js
+++ src/js/data/standarize.js
@@ -1,34 +1,45 @@
-import clone from '../utils';
-
-const sumarize = (data) => data.reduce((x, y) => x + y);
-
-const mean = (data) => sumarize(data) / data.length;
-
-const variance = (data, meanValue) => {
-  const sum = data.reduce((x, y) => x + (y - meanValue) ** 2);
-  return sum / data.length;
-};
-
-const standardDeviation = (data, meanValue) => Math.sqrt(variance(data, meanValue));
+import { mean, std, Matrix } from 'mathjs';
 
 function standarize(value, meanValue, standardDeviationValue) {
-  return (value - meanValue) / standardDeviationValue;
+	return (value - meanValue) / standardDeviationValue;
 }
 
-const standarizeData = (data, headers) => {
-  let result = clone(data);
-  headers.forEach((header) => {
-    const values = result.map((x) => parseFloat(x[header]));
-    const meanValue = mean(values);
-    const standardDeviationValue = standardDeviation(values, meanValue);
+const standarizeData = (data) => {
+	if (typeof Matrix !== typeof data) {
+		console.error('Data is not a matrix');
+		return;
+	}
 
-    result = result.map((x) => {
-      const newX = x;
-      newX[header] = standarize(parseFloat(newX[header]), meanValue, standardDeviationValue);
-      return newX;
-    });
-  });
-  return result;
+	let result = data.clone();
+	const [rows, cols] = result.size();
+	for (const colNumber of cols) {
+		const row = result.map((d) => d[colNumber]);
+		const meanValue = mean(row);
+		const standardDeviationValue = std(row);
+		result.map((value, [x, y], _) => {
+			if (colNumber !== y) {
+				return value;
+			}
+
+			return standarize(value, meanValue, standardDeviationValue);
+		});
+	}
+	// headers.forEach((header) => {
+	// 	const values = result.map((x) => parseFloat(x[header]));
+	// 	const meanValue = mean(values);
+	// 	const standardDeviationValue = std(values);
+
+	// 	result = result.map((x) => {
+	// 		const newX = x;
+	// 		newX[header] = standarize(
+	// 			parseFloat(newX[header]),
+	// 			meanValue,
+	// 			standardDeviationValue
+	// 		);
+	// 		return newX;
+	// 	});
+	// });
+	return result;
 };
 
 export default standarizeData;
