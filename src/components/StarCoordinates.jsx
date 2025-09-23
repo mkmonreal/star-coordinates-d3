7b93cc82d1a7d1d4a79538554c7eba5c0b293c48
Miguel Ángel Monreal Velasco <developer@mkmonreal.com>
2025-09-23 14:59:21 +0200

    WIP: props validation and quality code improvement

--- src/components/StarCoordinates.jsx
+++ src/components/StarCoordinates.jsx
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 import useConfigStore from '../stores/config-store';
 import { mod, matrix, multiply } from 'mathjs';
 import { buildCartesianVector } from '../utils/vector';
+import PropTypes from 'prop-types';
 
 const lineGenerator = d3.line();
 
@@ -18,6 +19,7 @@ function StarCoordinates({
 	const currentVectors = useRef(vectors);
 	const currentDataMatrix = useRef(dataMatrix);
 	const currentPoints = useRef([]);
+	const currentNode = useRef(null);
 	const unitCircleRadius = useConfigStore((state) => state.unitCircleRadius);
 	const arrowHeadScale = unitCircleRadius / 250;
 
@@ -89,153 +91,96 @@ function StarCoordinates({
 			.select('.arrows')
 			.selectAll('.arrow')
 			.data(currentVectors.current, (vector) => vector.label)
-			.join(
-				(enter) => {
-					enter
-						.append('g')
-						.classed('arrow', true)
-						.call((g) => {
-							g.append('path')
-								.classed('arrow-body', true)
-								.attr('d', (d) =>
-									lineGenerator(
-										getArrowbodyPath(
-											d.cartesian.x,
-											d.cartesian.y,
-											unitCircleRadius
-										)
-									)
-								)
-								.attr('stroke', 'gray');
-							g.append('path')
-								.classed('arrow-head', true)
-								.attr('d', (d) =>
-									lineGenerator(
-										getArrowheadPath(
-											{
-												x: d.cartesian.x * unitCircleRadius,
-												y: d.cartesian.y * unitCircleRadius,
-											},
-											arrowHeadScale
-										)
-									)
-								)
-								.attr('stroke', 'gray')
-								.attr('fill', 'gray')
-								.attr(
-									'transform',
-									(d) =>
-										`rotate(${getArrowheadRotation(d.cartesian.x, d.cartesian.y, d.polar.angle, unitCircleRadius)})`
-								)
-								.call((path) => {
-									path.call(
-										d3
-											.drag()
-											.on('start', () => {
-												path.style('cursor', 'grabbing');
-											})
-											.on('drag', function (e, d) {
-												const currentVector = currentVectors.current.find(
-													(vector) => d.label === vector.label
-												);
-												const newVector = buildCartesianVector(
-													currentVector.cartesian.x + e.dx / unitCircleRadius,
-													currentVector.cartesian.y - e.dy / unitCircleRadius,
-													currentVector.label
-												);
-												currentVectors.current = currentVectors.current.map(
-													(vector) =>
-														newVector.label === vector.label
-															? newVector
-															: vector
-												);
-
-												d3.select(this)
-													.attr(
-														'd',
-														lineGenerator(
-															getArrowheadPath(
-																{
-																	x: newVector.cartesian.x * unitCircleRadius,
-																	y: newVector.cartesian.y * unitCircleRadius,
-																},
-																arrowHeadScale
-															)
-														)
-													)
-													.attr(
-														'transform',
-														`rotate(${getArrowheadRotation(newVector.cartesian.x, newVector.cartesian.y, newVector.polar.angle, unitCircleRadius)})`
-													);
-												d3.select(this.parentNode)
-													.select('.arrow-body')
-													.attr(
-														'd',
-														lineGenerator(
-															getArrowbodyPath(
-																newVector.cartesian.x,
-																newVector.cartesian.y,
-																unitCircleRadius
-															)
-														)
-													);
-
-												const dataPoints = calculatePoints(
-													currentVectors.current,
-													currentDataMatrix.current
-												);
-
-												svg
-													.select('.data-circles')
-													.selectAll('.data-circle')
-													.data(dataPoints, (d) => d.id)
-													.join(
-														enterDataCircle,
-														updateDataCircle,
-														exitDataCircle
-													);
-											})
-											.on('end', () => {
-												path.style('cursor', 'grab');
-												onVectorUpdate(currentVectors.current);
-											})
-									);
-								});
-						});
-				},
-				(update) =>
-					update.call((g) => {
-						g.select('.arrow-body').attr('d', (d) =>
-							lineGenerator(
-								getArrowbodyPath(d.cartesian.x, d.cartesian.y, unitCircleRadius)
-							)
-						);
-						g.select('.arrow-head')
-							.attr('d', (d) =>
-								lineGenerator(
-									getArrowheadPath(
-										{
-											x: d.cartesian.x * unitCircleRadius,
-											y: d.cartesian.y * unitCircleRadius,
-										},
-										arrowHeadScale
-									)
-								)
-							)
-							.attr(
-								'transform',
-								(d) =>
-									`rotate(${getArrowheadRotation(d.cartesian.x, d.cartesian.y, d.polar.angle, unitCircleRadius)})`
-							);
-					}),
-				(exit) => exit.remove()
-			);
+			.join(enterArrows, updateArrows, exitArrows);
 
 		svg
 			.select('.data-circles')
 			.selectAll('.data-circle')
 			.data(currentPoints.current, (d) => d.id)
 			.join(enterDataCircle, updateDataCircle, exitDataCircle);
+
+		function enterArrows(enter) {
+			enter
+				.append('g')
+				.classed('arrow', true)
+				.call((g) => {
+					g.append('path')
+						.classed('arrow-body', true)
+						.attr('d', (d) =>
+							lineGenerator(
+								getArrowbodyPath(d.cartesian.x, d.cartesian.y, unitCircleRadius)
+							)
+						)
+						.attr('stroke', 'gray');
+					g.append('path')
+						.classed('arrow-head', true)
+						.attr('d', (d) =>
+							lineGenerator(
+								getArrowheadPath(
+									{
+										x: d.cartesian.x * unitCircleRadius,
+										y: d.cartesian.y * unitCircleRadius,
+									},
+									arrowHeadScale
+								)
+							)
+						)
+						.attr('stroke', 'gray')
+						.attr('fill', 'gray')
+						.attr(
+							'transform',
+							(d) =>
+								`rotate(${calculateArrowheadRotation(d.cartesian.x, d.cartesian.y, d.polar.angle, unitCircleRadius)})`
+						)
+						.call((path) => {
+							path.call(
+								d3
+									.drag()
+									.on('start', (e) => {
+										currentNode.current = e.sourceEvent.target;
+										path.style('cursor', 'grabbing');
+									})
+									.on('drag', handleOnDragArrowhead)
+									.on('end', () => {
+										currentNode.current = null;
+										path.style('cursor', 'grab');
+										onVectorUpdate(currentVectors.current);
+									})
+							);
+						});
+				});
+		}
+
+		function updateArrows(update) {
+			update.call((g) => {
+				g.select('.arrow-body').attr('d', (d) =>
+					lineGenerator(
+						getArrowbodyPath(d.cartesian.x, d.cartesian.y, unitCircleRadius)
+					)
+				);
+				g.select('.arrow-head')
+					.attr('d', (d) =>
+						lineGenerator(
+							getArrowheadPath(
+								{
+									x: d.cartesian.x * unitCircleRadius,
+									y: d.cartesian.y * unitCircleRadius,
+								},
+								arrowHeadScale
+							)
+						)
+					)
+					.attr(
+						'transform',
+						(d) =>
+							`rotate(${calculateArrowheadRotation(d.cartesian.x, d.cartesian.y, d.polar.angle, unitCircleRadius)})`
+					);
+			});
+		}
+
+		function exitArrows(exit) {
+			exit.remove();
+		}
 
 		function enterDataCircle(enter) {
 			enter
@@ -257,6 +202,66 @@ function StarCoordinates({
 		function exitDataCircle(exit) {
 			exit.remove();
 		}
+
+		function handleOnDragArrowhead(e, d) {
+			const currentVector = currentVectors.current.find(
+				(vector) => d.label === vector.label
+			);
+
+			const newVector = buildCartesianVector(
+				currentVector.cartesian.x + e.dx / unitCircleRadius,
+				currentVector.cartesian.y - e.dy / unitCircleRadius,
+				currentVector.label
+			);
+			currentVectors.current = currentVectors.current.map((vector) =>
+				newVector.label === vector.label ? newVector : vector
+			);
+
+			updateArrowPosition(
+				d3.select(currentNode.current),
+				newVector.cartesian.x,
+				newVector.cartesian.y,
+				newVector.polar.angle
+			);
+
+			const dataPoints = calculatePoints(
+				currentVectors.current,
+				currentDataMatrix.current
+			);
+
+			svg
+				.select('.data-circles')
+				.selectAll('.data-circle')
+				.data(dataPoints, (d) => d.id)
+				.join(enterDataCircle, updateDataCircle, exitDataCircle);
+		}
+
+		function updateArrowPosition(selection, x, y, angle) {
+			if (!selection.node()) {
+				return;
+			}
+
+			selection
+				.attr(
+					'd',
+					lineGenerator(
+						getArrowheadPath(
+							{
+								x: x * unitCircleRadius,
+								y: y * unitCircleRadius,
+							},
+							arrowHeadScale
+						)
+					)
+				)
+				.attr(
+					'transform',
+					`rotate(${calculateArrowheadRotation(x, y, angle, unitCircleRadius)})`
+				);
+			d3.select(selection.node().parentNode)
+				.select('.arrow-body')
+				.attr('d', lineGenerator(getArrowbodyPath(x, y, unitCircleRadius)));
+		}
 	}, [unitCircleRadius, arrowHeadScale, vectors, onVectorUpdate, dataMatrix]);
 
 	return <svg ref={svgRef}></svg>;
@@ -273,7 +278,7 @@ function getArrowheadPath({ x, y }, ratio = 1) {
 	];
 }
 
-function getArrowheadRotation(x, y, angle, unitCircleRadius = 1) {
+function calculateArrowheadRotation(x, y, angle, unitCircleRadius = 1) {
 	y = -y;
 	return `${mod(360 - angle, 360)} ${x * unitCircleRadius}, ${y * unitCircleRadius}`;
 }
@@ -306,5 +311,13 @@ function calculatePoints(vectors, dataMatrix) {
 
 	return calculatedPoints;
 }
+
+StarCoordinates.propTypes = {
+	height: PropTypes.number.isRequired,
+	width: PropTypes.number.isRequired,
+	vectors: PropTypes.array.isRequired,
+	onVectorUpdate: PropTypes.func.isRequired,
+	dataMatrix: PropTypes.object.isRequired,
+};
 
 export default StarCoordinates;
